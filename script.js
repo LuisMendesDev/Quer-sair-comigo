8cd6e4f2c43e8471b74364be4321376fa029e628
Luís Mendes <122231183+LuisMendesDev@users.noreply.github.com>
2024-03-05 15:46:29 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let button = document.getElementById('no');
-let height = window.innerHeight - 50;
-let width = window.innerWidth - 50;
-button.addEventListener('mouseover', function () {
-  button.style.position = "absolute";
-  button.style.top = Math.random() * height + "px";
-  button.style.left = Math.random() * width + "px";
-});
